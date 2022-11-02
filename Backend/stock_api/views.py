894d2a471fbf46ce18cfe3d4f275875f804a5685
Tristen Martinez <tristen.m1324@gmail.com>
2022-11-02 14:45:37 -0700

    working webscrapper for yahoo finance using stock tickers. BTC-USD does not work, getting a 404 error when trying to get it

--- Backend/stock_api/views.py
+++ Backend/stock_api/views.py
@@ -1,8 +1,9 @@
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 import mechanicalsoup
+from bs4 import BeautifulSoup
 
-
+import requests
 # @api_view(['GET'])
 # def get_stocks(request, stockQuery):
 #     print("THIS IS STOCK QUERY", stockQuery)
@@ -34,4 +35,54 @@ import mechanicalsoup
 
 @api_view(['GET'])
 def get_stocks(request, stockTicker):
-    page = f'https://finance.yahoo.com/quote/{stockTicker}?p={stockTicker}&.tsrc=fin-srch'
+    upperStockTicker = stockTicker.upper()
+    print(upperStockTicker)
+    try:
+        ResponseObject = {}
+        page = requests.get(f'https://finance.yahoo.com/quote/{upperStockTicker}?p={upperStockTicker}&.tsrc=fin-srch')
+        print(page)
+        
+        #create object
+        soup = BeautifulSoup(page.text, 'html.parser')
+        
+        #gets the element that holds the currentMarketPrice value
+        stock_element_price = soup.find("fin-streamer", {"data-symbol":{upperStockTicker}, "data-field": "regularMarketPrice"})
+        stock_price = stock_element_price.contents[0]
+        print(stock_price)
+
+        #get the stock name
+        stock_name_element = soup.find("h1")
+        stock_name = stock_name_element.contents[0]
+        
+        #get table data for stock
+
+        stock_table_element = soup.find("table")
+        table_data = stock_table_element.find_all('td')
+        
+        try:
+            for table_row in table_data:
+                
+                span_element = table_row.find_all('span')
+                try:
+                    temp_key = span_element[0].contents[0]
+                except:
+                    ResponseObject[temp_key] = table_row.contents[0]
+        except:
+            pass
+                
+        
+        
+
+        #building our responseObject
+        
+        ResponseObject["stock_name"] = stock_name
+        ResponseObject["ticker"] = f'${upperStockTicker}'
+        ResponseObject["price"] = stock_price
+        try:
+            del ResponseObject["Volume"]
+        except:
+            pass
+        return Response(ResponseObject)
+    except:
+        return Response({"message": "Query not found"})
+
