6c708c5ab404b91c0395e4fd7069bfbdaaf4aeb7
Tristen Martinez <tristen.m1324@gmail.com>
2022-11-02 11:50:21 -0700

    beautiful soup install

--- Backend/stock_api/urls.py
+++ Backend/stock_api/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("stock/<str:stockQuery>/", views.get_stocks, name="get-stock-info" ),
+    path("stock/<str:stockTicker>/", views.get_stocks, name="get-stock-info" ),
 ]
--- Backend/stock_api/views.py
+++ Backend/stock_api/views.py
@@ -3,30 +3,35 @@ from rest_framework.decorators import api_view
 import mechanicalsoup
 
 
+# @api_view(['GET'])
+# def get_stocks(request, stockQuery):
+#     print("THIS IS STOCK QUERY", stockQuery)
+#     browser = mechanicalsoup.StatefulBrowser()
+
+#     browser.open("https://finance.yahoo.com/")
+#     browser.select_form()
+#     browser.get_current_form().print_summary()
+
+#     # search_term = input("What would you like to search for?")
+#     search_term = stockQuery
+#     browser["yfin-usr-qry"] = search_term
+
+
+#     browser.launch_browser()
+#     response = browser.submit_selected()
+
+#     print('new url:', browser.get_url())
+#     print('my response:', response.text[:500])
+
+
+#     #getting the stock page data
+#     new_url = browser.get_url()
+#     browser.open(new_url)
+
+#     #getting the html
+#     page = browser.get_current_page()
+
+
 @api_view(['GET'])
-def get_stocks(request, stockQuery):
-    print("THIS IS STOCK QUERY", stockQuery)
-    browser = mechanicalsoup.StatefulBrowser()
-
-    browser.open("https://finance.yahoo.com/")
-    browser.select_form()
-    browser.get_current_form().print_summary()
-
-    # search_term = input("What would you like to search for?")
-    search_term = stockQuery
-    browser["yfin-usr-qry"] = search_term
-
-
-    browser.launch_browser()
-    response = browser.submit_selected()
-
-    print('new url:', browser.get_url())
-    print('my response:', response.text[:500])
-
-
-    #getting the stock page data
-    new_url = browser.get_url()
-    browser.open(new_url)
-
-    #getting the html
-    page = browser.get_current_page()
+def get_stocks(request, stockTicker):
+    page = f'https://finance.yahoo.com/quote/{stockTicker}?p={stockTicker}&.tsrc=fin-srch'
