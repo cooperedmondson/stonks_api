cef91f3bde5a2a531022e0c4f7d89508f4956a33
Cooper Edmondson <cooperedmondson0115@gmail.com>
2022-11-03 01:29:41 -0400

    Finished Secondary Stock Api View

--- Backend/stock_api/urls.py
+++ Backend/stock_api/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path("stock/<str:stockTicker>/", views.get_stocks, name="get-stock-info"),
+    path("stonk/<str:stockQuery>/", views.get_stonks, name="get-stock" ),
 ]
--- Backend/stock_api/views.py
+++ Backend/stock_api/views.py
@@ -2,35 +2,60 @@ from rest_framework.response import Response
 from rest_framework.decorators import api_view
 import mechanicalsoup
 from bs4 import BeautifulSoup
-
 import requests
-# @api_view(['GET'])
-# def get_stocks(request, stockQuery):
-#     print("THIS IS STOCK QUERY", stockQuery)
-#     browser = mechanicalsoup.StatefulBrowser()
 
-#     browser.open("https://finance.yahoo.com/")
-#     browser.select_form()
-#     browser.get_current_form().print_summary()
+@api_view(['GET'])
+def get_stonks(request, stockQuery):
+    try:
+        ResponseObject = {}
+        print("THIS IS STOCK QUERY", stockQuery)
 
-#     # search_term = input("What would you like to search for?")
-#     search_term = stockQuery
-#     browser["yfin-usr-qry"] = search_term
+        browser = mechanicalsoup.StatefulBrowser()
+        browser.open("https://finance.yahoo.com/")
+        browser.select_form()
 
+        search_term = stockQuery
+        browser["yfin-usr-qry"] = search_term
+        response = browser.submit_selected()
+        new_url = browser.get_url()
+        browser.open(str(new_url))
+        print('new url:', browser.get_url())
+        print('my response:', response.text[:500])
 
-#     browser.launch_browser()
-#     response = browser.submit_selected()
+        
+        headers={'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.61 Safari/537.36'}
+        page = requests.get(new_url, headers=headers).text
+        soup = BeautifulSoup(page, 'html.parser')
+        price = soup.find('fin-streamer', {'class': 'Fw(b) Fz(36px) Mb(-4px) D(ib)'})
+        stock_price = price.contents[0]
+        
 
-#     print('new url:', browser.get_url())
-#     print('my response:', response.text[:500])
+        stock_name_element = soup.find("h1")
+        stock_name = stock_name_element.contents[0]
 
+        stock_table_element = soup.find("table")
+        table_data = stock_table_element.find_all('td')
 
-#     #getting the stock page data
-#     new_url = browser.get_url()
-#     browser.open(new_url)
+        try:
+            for table_row in table_data:
+                span_element = table_row.find_all('span')
+                try:
+                    temp_key = span_element[0].contents[0]
+                except:
+                    ResponseObject[temp_key] = table_row.contents[0]
+        except:
+            pass
+        ResponseObject["stock_name"] = stock_name
+        ResponseObject["stock_url"] = new_url
+        ResponseObject["price"] = stock_price
+        try:
+            del ResponseObject["Volume"]
+        except:
+            pass
+        return Response(ResponseObject)
+    except:
+        return Response({"message": "Query not found"})
 
-#     #getting the html
-#     page = browser.get_current_page()
 
 
 @api_view(['GET'])
